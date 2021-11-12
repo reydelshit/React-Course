79367a1fd577f2983c1d5d71326cf8156692b336
Reydel Ocon <reydel321@gmail.com>
2021-11-12 20:31:11 +0800

    luh

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -23,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by { blog.author }</p>
                     <div>{blog.body}</div>
+                    <p>yow</p>
                     <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
